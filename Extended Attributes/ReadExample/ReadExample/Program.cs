719aa38ec6250857457f557d5df3d748ae73e30a
AsposeSamples <artemkosonog@gmail.com>
2018-11-28 11:50:47 +0200

    Aspose.Tasks Samples

---
+++ Extended Attributes/ReadExample/ReadExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Aspose.Tasks;
+
+namespace ReadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "TestProject.mpp");
+
+            Project myProject = new Project(filePath);
+
+            
+            TaskCollection tasks = myProject.RootTask.Children;
+
+            Task[] serverTasks = tasks.Where(t => t.ExtendedAttributes.Any(ea => ea.TextValue.StartsWith("Server"))).ToArray();
+
+            Task[] clientTasks = tasks.Where(t => t.ExtendedAttributes.Any(ea => ea.TextValue.StartsWith("Client"))).ToArray();
+
+
+            if (serverTasks.Any())
+            {
+                Console.WriteLine($"Server Side: {string.Join(", ", serverTasks.Select(t => t.Get(Tsk.Name)))}");
+            }
+
+            if (clientTasks.Any())
+            {
+                Console.WriteLine($"Client Side: {string.Join(", ", clientTasks.Select(t => t.Get(Tsk.Name)))}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Extended Attributes/WriteExample/WriteExample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using Aspose.Tasks;
+using Aspose.Tasks.Saving;
+
+namespace WriteExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "TestProject.mpp");
+
+            Project myProject = new Project(filePath);
+
+
+            ExtendedAttributeDefinition attribute = ExtendedAttributeDefinition.CreateLookupTaskDefinition(CustomFieldType.Text, ExtendedAttributeTask.Text1, "Test Attribute");
+
+            Value value1 = new Value
+            {
+                Id = 1,
+                StringValue = "Server/Database",
+                Description = "DataBase of Server Side"
+            };
+
+            Value value2 = new Value
+            {
+                Id = 2,
+                StringValue = "Server/Api",
+                Description = "Api of Server Side"
+            };
+
+            Value value3 = new Value
+            {
+                Id = 3,
+                StringValue = "Client/WebUI",
+                Description = "WebUI of Client Side"
+            };
+
+            attribute.AddLookupValue(value1);
+
+            attribute.AddLookupValue(value2);
+
+            attribute.AddLookupValue(value3);
+
+            myProject.ExtendedAttributes.Add(attribute);
+
+
+            ExtendedAttribute serverDatabaseAttribute = attribute.CreateExtendedAttribute(value1);
+
+            ExtendedAttribute serverApiAttribute = attribute.CreateExtendedAttribute(value2);
+
+            ExtendedAttribute clientUIAttribute = attribute.CreateExtendedAttribute(value3);
+
+
+            Task task1 = myProject.RootTask.Children.Add("Task 1");
+
+            task1.ExtendedAttributes.Add(serverDatabaseAttribute);
+
+
+            Task task2 = myProject.RootTask.Children.Add("Task 2");
+
+            task2.ExtendedAttributes.Add(serverDatabaseAttribute);
+
+
+            Task task3 = myProject.RootTask.Children.Add("Task 3");
+
+            task3.ExtendedAttributes.Add(serverApiAttribute);
+
+
+            Task task4 = myProject.RootTask.Children.Add("Task 4");
+
+            task4.ExtendedAttributes.Add(clientUIAttribute);
+
+            myProject.Save(filePath, SaveFileFormat.MPP);
+        }
+    }
+}
